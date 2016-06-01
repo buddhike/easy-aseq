92eeb6b42f51858c2d66708d68fc6bca615bc4af
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-06-01 16:56:54 +1000

    Support non-async mappters.

--- test/map.js
+++ test/map.js
@@ -7,4 +7,18 @@ describe('map', () => {
       r.length.should.equal(0);
     });
   });
+
+  describe('non async map', () => {
+    it('should return the mapped value', async () => {
+      const r = await map([1], i => i);
+      r.should.eql([1]);
+    });
+  });
+
+  describe('an async map', () => {
+    it('should return the mapped value', async () => {
+      const r = await map([1], async i => i);
+      r.should.eql([1]);
+    });
+  });
 });
